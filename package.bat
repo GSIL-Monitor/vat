b01ed4b11017f24b82f63ecf04bd8e107f100f80
scdvalserver <scdvalserver@tcl.com>
2018-06-12 18:03:27 +0800

    update package

--- package.bat
+++ package.bat
@@ -1,4 +1,4 @@
 cd %cd%
 echo %cd%
-pyinstaller -w %cd%\vat.py %cd%\vatgui.py %cd%\icon.py -i %cd%\resource\gui\vat.ico
+pyinstaller -w %cd%\vat.py %cd%\vatgui.py %cd%\icon_rc.py -i %cd%\resource\gui\vat.ico
 pause
